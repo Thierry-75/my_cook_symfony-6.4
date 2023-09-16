3ffb66880bfc227d2345bde2e83d7d1de59619c9
thierry dothee <thierrydothee@protonmail.com>
2023-09-16 09:49:52 +0200

    creation email

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,17 +4,20 @@ namespace App\Controller;
 
 use App\Entity\Contact;
 use App\Form\ContactType;
+use Symfony\Component\Mime\Email;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact', methods:['GET','POST'])]
-    public function index(Request $request,EntityManagerInterface $em, ValidatorInterface $validator ): Response
+    public function index(Request $request,EntityManagerInterface $em, ValidatorInterface $validator, MailerInterface $mailer ): Response
     {
         $contact = new Contact();
         if($this->getUser()){
@@ -34,6 +37,20 @@ class ContactController extends AbstractController
                 if($form->isSubmitted() && $form->isValid()){
                     $em->persist($contact);
                     $em->flush();
+                    //email
+                    $email = (new Email())
+                    ->from($contact->getEmail())
+                    ->to('admin@cook.fr')
+                    //->cc('cc@example.com')
+                    //->bcc('bcc@example.com')
+                    //->replyTo('fabien@example.com')
+                    ->priority(Email::PRIORITY_HIGH)
+                    ->subject($contact->getSubject())
+                    ->text($contact->getMessage()." " . $contact->getFullName());
+                   // ->html(."<br />". $contact->getFullName());
+        
+                $mailer->send($email);
+
                     $this->addFlash('success', 'Votre demande a bien été enrregistrée !');
                     return $this->redirectToRoute('app_main');
                 }
